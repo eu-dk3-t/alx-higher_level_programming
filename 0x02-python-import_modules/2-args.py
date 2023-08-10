8f4ec8dd64d4481c96618d82da3b61c91a7e74ad
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-10 17:38:07 +0000

    Prints num and list of args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# File name:
+#       2-args.py
+# Description:
+#       Prints the number of and
+#       list of arguments
+# Author:
+#       eu-dk3-t
+
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
