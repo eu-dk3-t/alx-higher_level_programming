943b0e400ee14d9df5c3e671a67b9cda9c56d8ec
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-10 17:43:25 +0000

    Imports var a from the variable_load_5.py

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# File name:
+#       5-variable_load.py
+# Description:
+#       Imports var a from the
+#       variable_load_5.py
+# Author:
+#       eu-dk3-t
+
+from variable_load_5 import a
+
+if __name__ == '__main__':
+    print(a)
