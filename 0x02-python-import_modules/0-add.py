fdf7dfc741001e5c0140aa21367427756822742a
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-10 17:35:53 +0000

    Imports add from add_0.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# File name:
+#       0-add.py
+# Description:
+#       Imports the function add
+#       from the file add_0.py
+# Author:
+#       eu-dk3-t
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
