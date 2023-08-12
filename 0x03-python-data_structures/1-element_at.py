a1839bed661bef930b4d4e062b177c68a405b351
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-12 17:08:03 +0000

    Retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# File name:
+#       1-element_at.py
+# Description:
+#       Retrieves an element from a list
+# Author:
+#       eu-dk3-t
+
+
+def element_at(my_list, idx):
+     if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
