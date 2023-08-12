9ad335211461f412b84763ae3ea65c59a2431543
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-12 17:30:44 +0000

    Prints a matrix of ints

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# File name:
+#       6-print_matrix_integer.py
+# Description:
+#       Prints a matrix of integers
+# Author:
+#       eu-dk3-t
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
