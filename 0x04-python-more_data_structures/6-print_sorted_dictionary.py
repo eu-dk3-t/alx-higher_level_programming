fb9515194f275f0bd205a569f13e2eb6faa18f3f
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-16 17:54:50 +0000

    Replaced str.format with f-strings

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -8,4 +8,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    [print("{}: {}".format(key, a_dictionary[key])) for key in sorted(a_dictionary)]
+    [print(f"{key}: {a_dictionary[key]}") for key in sorted(a_dictionary)]
+
