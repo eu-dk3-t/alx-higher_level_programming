343000ba19edd914a492645d7cb50c23c74aa20e
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-16 17:20:15 +0000

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File name:
+#       4-only_diff_elements.py
+# Description:
+#       Returns a set of unique elements
+#       found in either of two sets
+# Author:
+#       eu-dk3-t
+
+
+def only_diff_elements(set_1, set_2):
+    return (set_1 ^ set_2)
