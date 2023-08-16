3afc41376f09c55086434c7a3994dd94520ae35d
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-16 17:29:46 +0000

    Returns num of keys in a dict

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File name:
+#       5-number_keys.py
+# Description:
+#       Returns num of keys in a dict.
+# Author:
+#       eu-dk3-t
+
+
+def number_keys(a_dictionary):
+    return (len(a_dictionary))
