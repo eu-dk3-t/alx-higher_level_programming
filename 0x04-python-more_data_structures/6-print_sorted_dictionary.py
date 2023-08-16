3931b2dd5d72d0f6c9e75ccb8cde3431887617fa
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-16 17:20:15 +0000

    Prints a dict by ordered keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File name:
+#       6-print_sorted_dictionary.py
+# Description:
+#       Prints a dict by ordered keys
+# Author:
+#       eu-dk3-t
+
+
+def print_sorted_dictionary(a_dictionary):
+    [print("{}: {}".format(key, a_dictionary[key])) for key in sorted(a_dictionary)]
