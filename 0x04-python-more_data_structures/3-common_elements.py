058e0e5a91dec23177cb34df101a85cfb3cddc1a
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-16 17:27:52 +0000

    Returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File name:
+#       3-common_elements.py
+# Description:
+#       Returns a set of common elements in two sets
+# Author:
+#       eu-dk3-t
+
+
+def common_elements(set_1, set_2):
+    return (set_1 & set_2)
