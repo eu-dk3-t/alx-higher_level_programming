220832d105001e13691240067292ea899a042407
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-29 16:34:56 +0000

    Bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+import math
+
+
+class MagicClass:
+    """Represent a circle."""
+
+    def __init__(self, radius=0):
+        """
+        Function nameL
+            __init__
+        Description:
+            Initialization
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """
+        Function name:
+            area
+        Description:
+                Return the area of the MagicClass, circle
+        """
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        """
+        Function name:
+            circumference
+        Description:
+                Return The circumference of the MagicClass, circle
+        """
+        return (2 * math.pi * self.__radius)
