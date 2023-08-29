f56a2453b04e9b82351cdb64e3f6426833b767f7
eu-dk3-t <ze_3fanuel@proton.me>
2023-08-29 17:21:17 +0000

    Prints the first x elements of a list, only ints

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """ Prints x elements of a list. Only integers"""
+    printed = 0
+
+    for i in range(x):
+        try:
+            if type(my_list[i]) is int and printed != x:
+                print('{:d}'.format(my_list[i]), end='')
+                printed += 1
+        except TypeError:
+            continue
+
+    print()
+
+    return printed
