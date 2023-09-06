aa8db59118416f20f54cf0ccc6e9c7309eb309ee
eu-dk3-t <ze_3fanuel@proton.me>
2023-09-06 18:42:52 +0000

    5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Defines a text-indentation function """
+
+
+def text_indentation(text):
+    """
+    Function name:
+        text_indentation
+    Description:
+        Print text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (string): The text to print.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    no_space = True
+    new_text = ''
+
+    for char in text.strip():
+        if char == ' ' and no_space:
+            pass
+        elif char in ':?.':
+            new_text += char + "\n\n"
+            no_space = True
+        else:
+            new_text += char
+            no_space = False
+
+    print(new_text, end='')
