a750b9dec8ae9fdcb62582d326bcff4c7eee2560
eu-dk3-t <ze_3fanuel@proton.me>
2023-09-11 16:10:16 +0000

    Checks if isInstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,6 +5,6 @@ def is_same_class(obj, a_class):
     """
     Checks if two objects are EXACTLY the same class
     """
-    if not isinstance(a_class, type):
-        raise TypeError("a_class must be of type 'type'")
-    return (type(obj) is a_class)
+    if type(obj) != a_class:
+        return False
+    return True
