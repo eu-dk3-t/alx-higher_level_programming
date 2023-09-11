c713e5e251d73d2495ba78aaf5af5669fe8301fe
eu-dk3-t <ze_3fanuel@proton.me>
2023-09-11 15:34:18 +0000

    BaseGeometry Class

---
+++ 0x0B-python-input_output/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module containing the `BaseGeometry` class. """
+
+
+class BaseGeometry:
+    """ Base Geometry Class """
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
