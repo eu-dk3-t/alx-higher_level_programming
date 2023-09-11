8f857f276c3e878084d3422b54457ce88dd15670
eu-dk3-t <ze_3fanuel@proton.me>
2023-09-11 15:36:21 +0000

    Adds or Modifies an attribute

---
+++ 0x0B-python-input_output/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Module containing `add_attribute` function. """
+
+
+def add_attribute(obj, name, value):
+    """
+    Adds/Modifies an instance attribute if possible.
+    Args:
+        obj: An instance object.
+        name: Name of the attribute.
+        value: The value of the attribute.
+    """
+    if "__dict__" in dir(obj):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
