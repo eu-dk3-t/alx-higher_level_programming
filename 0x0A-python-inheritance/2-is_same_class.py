1fa3430aa10ce1b1c7604ba3201fffd777954dba
eu-dk3-t <ze_3fanuel@proton.me>
2023-09-11 17:00:03 +0000

    Documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,8 +2,15 @@
 
 
 def is_same_class(obj, a_class):
+    """ Checks if two objects are EXACTLY the same class """
+
     """
-    Checks if two objects are EXACTLY the same class
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        isInstance - True
+        Otherwise - False.
     """
     if type(obj) != a_class:
         return False
